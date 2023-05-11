d11ff8c2c1d61a1527fd14048a89cc9ecd92a4ba
FelixFelicis555 <perfectpawi13@gmail.com>
2023-05-12 00:08:24 +0530

    'Mutual-Funds-App'

---
+++ src/service/INDService.ts
@@ -0,0 +1,30 @@
+
+const MutualFundData=require('../contract/testData.json');
+
+function getRequestUrl(offset:number){
+    return  `https://dev.indiawealth.in/api/v2/funds/getList/?limit=16&offset=${offset}`;
+}
+
+const AuthToken = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMDI3LCJ1c2VybmFtZSI6Ijk4MTE4ODU5ODkiLCJleHAiOjE1NTE3NzA1MzcsImVtYWlsIjoia2xzYWRqbGFAYXNkLmFjb20iLCJtb2JpbGUiOiI5ODExODg1OTg5In0.yNnIloeXKJk5q77osuieKAWR-1vhAjpGbjdTh-u3A7E`;
+
+export class IndWealth {
+
+    static async getMutualFunds(offset:number): Promise<Response> {
+    
+        return fetch(getRequestUrl(offset),
+        {
+            method:`get`,
+            headers:{
+                "Authorization": AuthToken
+            }
+        }  )
+ };
+
+ static async getMutualFundsOffline(offset:number) : Promise<Response>{
+
+    let mutualFundData={...MutualFundData};
+    mutualFundData.data=mutualFundData.data.slice(offset,offset+16);
+    return Promise.resolve(mutualFundData);
+ };
+ 
+}
